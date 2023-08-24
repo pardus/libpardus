aef14e087305fe67e94230fffaa324531fe19cc5
qwe <osman.coskun@pardus.org.tr>
2023-08-25 00:06:38 +0300

    fix: new folder and module structure

---
+++ libpardus/Ptk.py
@@ -0,0 +1,35 @@
+from .bin.Ptk.AboutWindow import AboutWindow
+from .bin.Ptk.App import App
+from .bin.Ptk.ApplicationWindow import ApplicationWindow
+from .bin.Ptk.Box import Box
+from .bin.Ptk.Button import Button
+from .bin.Ptk.Flowbox import FlowBox
+from .bin.Ptk.Image import Image
+from .bin.Ptk.Label import Label
+from .bin.Ptk.ListBox import ListBox
+from .bin.Ptk.ListBoxRow import ListBoxRow
+from .bin.Ptk.Scale import Scale
+from .bin.Ptk.ScrolledWindow import ScrolledWindow
+from .bin.Ptk.Separator import Separator
+from .bin.Ptk.Stack import Stack
+from .bin.Ptk.ToggleButton import ToggleButton
+from .bin.Ptk.utils import utils
+
+__all__ = [
+    "AboutWindow",
+    "App",
+    "ApplicationWindow",
+    "Box",
+    "Button",
+    "Flowbox",
+    "Image",
+    "Label",
+    "ListBox",
+    "ListBoxRow",
+    "Scale",
+    "ScrolledWindow",
+    "Separator",
+    "Stack",
+    "ToggleButton",
+    "utils",
+]
--- libpardus/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-libpardus
-
-Pardus Library for Pardus Applications and Utilities.
-"""
-
-__version__ = "0.0.1"
-__author__ = 'Osman Coskun'
-__credits__ = 'TUBITAK / ULAKBIM / PARDUS'
